163c462fb1fc70f16ff16dc322b470fcdc4a86ee
TomorrowWontExist <jay@tomorrowwontexist.com>
2022-08-14 09:58:17 -0500

    Dev Help

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
+import i11 from "./assets/images/aaaa.png";
+import i90 from "./assets/images/9099.png";
+import i97 from "./assets/images/97.png";
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
@@ -16,14 +19,14 @@ export const StyledButton = styled.button`
   padding: 10px;
   font-weight: bold;
   color: var(--secondary-text);
-  width: 100px;
+  width: 200px;
   cursor: pointer;
   box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
   -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
   -moz-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  :active {
+  :hover {
     box-shadow: none;
-    -webkit-box-shadow: none;
+    -webkit-box-shadow: 0px 4px 0px 1px rgba(0, 0, 0, 1);
     -moz-box-shadow: none;
   }
 `;
@@ -53,6 +56,39 @@ export const StyledRoundButton = styled.button`
   }
 `;
 
+export const Gallery = styled.div`
+  height: 0px;
+  position: fixed;
+  margin-bottom: 0px;
+
+  .photobanner {
+    position: fixed;
+    top: 0px;
+    right: 0px;
+    overflow: hidden;
+    white-space: nowrap;
+    animation: bannermove 60s linear infinite alternate;
+
+    &:hover {
+      animation-play-state: ;
+    }
+  }
+
+  .photobanner img {
+    height: 200px;
+    margin: 0 .0em;
+  }
+
+  @keyframes bannermove {
+    70% {
+      transform: translate( -50%, 0);
+    }
+    70% {
+      transform: translate( 50%, 0);
+    }
+  }
+`;
+
 export const ResponsiveWrapper = styled.div`
   display: flex;
   flex: 1;
@@ -63,6 +99,23 @@ export const ResponsiveWrapper = styled.div`
   @media (min-width: 767px) {
     flex-direction: row;
   }
+`;
+
+export const StyledImg4 = styled.img`
+border-radius: 20px;
+color: #ffffff;
+cursor: pointer;
+box-shadow: 2px 8px 4px -2px rgba(100, 0, 250, 0.0);
+-webkit-box-shadow: 2px 3px 10px -2px rgba(100, 0, 250, 0);
+-moz-box-shadow: 2px 8px 4px -2px rgba(100, 0, 250, 0.0);
+:active {
+  box-shadow: none;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+}
+:hover {
+  -webkit-box-shadow: 2px 3px 20px -2px rgba(250, 250, 0, 0.0);
+}
 `;
 
 export const StyledLogo = styled.img`
@@ -92,6 +145,10 @@ export const StyledImg = styled.img`
 export const StyledLink = styled.a`
   color: var(--secondary);
   text-decoration: none;
+  :hover {
+    box-shadow: none;
+    -webkit-box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 1);
+    -moz-box-shadow: none;
 `;
 
 function App() {
@@ -99,7 +156,8 @@ function App() {
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
-  const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
+  const [feedback, setFeedback] = useState(`Mint 5+ Bees to adopt a Beehive and earn royalties for life!`);
+  // const [feedback, setFeedback] = useState(`"We aren't anti-social... We're anti-low vibing energy, anti-fakeness, and anti-bullsh*t."`);
   const [mintAmount, setMintAmount] = useState(1);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
@@ -124,10 +182,10 @@ function App() {
     let cost = CONFIG.WEI_COST;
     let gasLimit = CONFIG.GAS_LIMIT;
     let totalCostWei = String(cost * mintAmount);
-    let totalGasLimit = String(gasLimit * mintAmount);
+    let totalGasLimit = String(gasLimit + mintAmount * 3000);
     console.log("Cost: ", totalCostWei);
     console.log("Gas limit: ", totalGasLimit);
-    setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
+    setFeedback(`Preparing your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
       .mint(mintAmount)
@@ -139,13 +197,23 @@ function App() {
       })
       .once("error", (err) => {
         console.log(err);
-        setFeedback("Sorry, something went wrong please try again later.");
+        setFeedback("It seems the transaction was cancelled or something went wrong, please try again.");
         setClaimingNft(false);
       })
       .then((receipt) => {
         console.log(receipt);
         setFeedback(
-          `WOW, the ${CONFIG.NFT_NAME} is yours! go visit Opensea.io to view it.`
+          <s.TextDescription
+              style={{
+                textAlign: "center",
+                color: "var(--primary-text)",
+              }}
+            >
+              Woohoo! Your Bee NFTs are swarming at<p/>' 
+              <StyledLink target={"_blank"} href={CONFIG.MARKETPLACE_LINK}>
+                {truncate(CONFIG.MARKETPLACE, 20)}
+              </StyledLink>'
+            </s.TextDescription>
         );
         setClaimingNft(false);
         dispatch(fetchData(blockchain.account));
@@ -162,8 +230,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 10) {
-      newMintAmount = 10;
+    if (newMintAmount > 23) {
+      newMintAmount = 23;
     }
     setMintAmount(newMintAmount);
   };
@@ -198,14 +266,48 @@ function App() {
       <s.Container
         flex={1}
         ai={"center"}
-        style={{ padding: 24, backgroundColor: "var(--primary)" }}
-        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
+        style={{ padding: 0, backgroundColor: "var(--primary)" }}
+        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : true}
       >
-        <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
+        <Gallery>
+          <div className='photobanner'>
+          <a href="https://thebeecollab.com/roadmap">
+            <img src={i90} alt='' />
+            {/* <img src={i25} alt='' />
+            <img src={i24} alt='' />
+            <img src={i23} alt='' />
+            <img src={i22} alt='' /> */}
+            <img src={i11} alt='' />
+            {/* <img src={i31} alt='' />
+            <img src={i30} alt='' />
+            <img src={i29} alt='' />
+            <img src={i28} alt='' />
+            <img src={i27} alt='' /> */}
+            <img src={i90} alt='' />
+            {/* <img src={i14} alt='' />
+            <img src={i15} alt='' />
+            <img src={i16} alt='' />
+            <img src={i17} alt='' />
+            <img src={i18} alt='' /> */}
+            <img src={i11} alt='' />
+            {/* <img src={i19} alt='' />
+            <img src={i20} alt='' />
+            <img src={i21} alt='' />
+            <img src={i26} alt='' /> */}
+            <img src={i90} alt='' />
+            </a>
+          </div>
+        </Gallery>
+        <div>
+<a href="https://thebeecollab.com/roadmap">
+<StyledImg4 src={i97} style={{ width: 500, height: 200, padding: 0 }}/>
+</a>
+</div>
+        {/* <StyledLogo alt={"logo"} src={"/config/images/97.png"} /> */}
         <s.SpacerSmall />
-        <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
+        <ResponsiveWrapper flex={1} style={{ padding: 40 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/example.gif"} />
+            <StyledImg alt={"example"} src={"/config/images/aa.gif"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
@@ -215,43 +317,86 @@ function App() {
             style={{
               backgroundColor: "var(--accent)",
               padding: 24,
-              borderRadius: 24,
+              borderRadius: 100,
               border: "4px dashed var(--secondary)",
               boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
             }}
           >
-            <s.TextTitle
+                       <s.TextTitle
               style={{
                 textAlign: "center",
-                fontSize: 50,
+                fontSize: 36,
                 fontWeight: "bold",
                 color: "var(--accent-text)",
               }}
             >
-              {data.totalSupply} / {CONFIG.MAX_SUPPLY}
+            The Bee Collab
             </s.TextTitle>
-            <s.TextDescription
+            <s.TextTitle
               style={{
                 textAlign: "center",
-                color: "var(--primary-text)",
+                fontSize: 16,
+                fontWeight: "bold",
+                color: "var(--accent-text)",
               }}
             >
-              <StyledLink target={"_blank"} href={CONFIG.SCAN_LINK}>
-                {truncate(CONFIG.CONTRACT_ADDRESS, 15)}
-              </StyledLink>
-            </s.TextDescription>
+            Autistic Adventures Farm is the name of our Farm that will house The Bee Collab Sanctuary.
+            Besides Saving the Bees with NFTs and making delicious Honey to share with our Investors, 
+            Autistic Adventures Farm will bee an Education Center that helps Neuro Divergent children 
+            be more comfortable in their own skin. Together we will spread the importance of Bees 
+            worldwide and partner with Farmlands to maximize crop production and expand our Honey operation! 
+            Eventually, every NFT will represent a royalty producing IRL Beehive, and be paid out in our
+            deflationary community Token governed by a DAO.<p/>
+            <s.TextDescription style={{ textAlign: "center" }}>
+                   {" "}
+                  <a
+                    target={""}
+                    href={"https://discord.gg/thebeecollab"}
+                  >
+                   Join Discord for more Info
+                  </a>
+                </s.TextDescription>
+                <s.TextDescription style={{ textAlign: "center" }}>
+                   {" "}
+                  <a
+                    target={""}
+                    href={"https://opensea.io/collection/autistic-adventures"}
+                  >
+                   View Collection on Opensea
+                  </a>
+                </s.TextDescription>
+                <s.TextDescription style={{ textAlign: "center" }}>
+                   {" "}
+                  <a
+                    target={""}
+                    href={"https://thebeecollab.com/roadmap"}
+                  >
+                   Study The Bee Collab Roadmap
+                  </a>
+                </s.TextDescription>
+            </s.TextTitle>
+            {/* <s.TextTitle
+              style={{
+                textAlign: "center",
+                fontSize: 20,
+                fontWeight: "bold",
+                color: "var(--accent-text)",
+              }}
+            >
+              {data.totalSupply} / 2000 Phase 1
+            </s.TextTitle> */}
             <s.SpacerSmall />
             {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
               <>
                 <s.TextTitle
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
-                  The sale has ended.
+                  This collection has sold out.
                 </s.TextTitle>
                 <s.TextDescription
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
-                  You can still find {CONFIG.NFT_NAME} on
+                  You can still trade {CONFIG.NFT_NAME} at
                 </s.TextDescription>
                 <s.SpacerSmall />
                 <StyledLink target={"_blank"} href={CONFIG.MARKETPLACE_LINK}>
@@ -264,13 +409,13 @@ function App() {
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
                   1 {CONFIG.SYMBOL} costs {CONFIG.DISPLAY_COST}{" "}
-                  {CONFIG.NETWORK.SYMBOL}.
+                  {CONFIG.NETWORK.SYMBOL} + Gas.
                 </s.TextTitle>
                 <s.SpacerXSmall />
                 <s.TextDescription
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
-                  Excluding gas fees.
+                  Gas is highly optimized and very cheap
                 </s.TextDescription>
                 <s.SpacerSmall />
                 {blockchain.account === "" ||
@@ -292,7 +437,7 @@ function App() {
                         getData();
                       }}
                     >
-                      CONNECT
+                      CONNECT WALLET
                     </StyledButton>
                     {blockchain.errorMsg !== "" ? (
                       <>
@@ -360,7 +505,9 @@ function App() {
                           getData();
                         }}
                       >
-                        {claimingNft ? "BUSY" : "BUY"}
+                        {claimingNft ? "BUZZY..." : "Mint Now"}
+                        {/* {claimingNft ? "YOU WILL WASTE GAS" : "STOP - WE ARE NOT LIVE"} */}
+
                       </StyledButton>
                     </s.Container>
                   </>
@@ -374,33 +521,39 @@ function App() {
             <StyledImg
               alt={"example"}
               src={"/config/images/example.gif"}
-              style={{ transform: "scaleX(-1)" }}
+              style={{ transform: "scaleX(1)" }}
             />
           </s.Container>
         </ResponsiveWrapper>
         <s.SpacerMedium />
         <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
-          <s.TextDescription
+        <s.TextDescription
+              style={{
+                textAlign: "center",
+                color: "var(--primary-text)",
+              }}
+            >
+              Contract Address: <StyledLink target={"_blank"} href={CONFIG.SCAN_LINK}>
+                {truncate(CONFIG.CONTRACT_ADDRESS, 42)}
+              </StyledLink>
+            </s.TextDescription>
+            <s.SpacerMedium />
+          {/* <s.TextDescription
             style={{
               textAlign: "center",
               color: "var(--primary-text)",
             }}
           >
-            Please make sure you are connected to the right network (
-            {CONFIG.NETWORK.NAME} Mainnet) and the correct address. Please note:
-            Once you make the purchase, you cannot undo this action.
-          </s.TextDescription>
-          <s.SpacerSmall />
-          <s.TextDescription
+            Please do not change the gas, we utilized ERC721A contract so you can mint up to 23 NFTs for the price of 2.  
+          </s.TextDescription> */}
+          {/* <s.TextDescription
             style={{
               textAlign: "center",
               color: "var(--primary-text)",
             }}
           >
-            We have set the gas limit to {CONFIG.GAS_LIMIT} for the contract to
-            successfully mint your NFT. We recommend that you don't lower the
-            gas limit.
-          </s.TextDescription>
+            Mobile users must open TheBeeCollab.io from MetaMask Browser to mint an NFT
+          </s.TextDescription> */}
         </s.Container>
       </s.Container>
     </s.Screen>
